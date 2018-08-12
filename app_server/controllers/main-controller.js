460c1bcfd753b7e7983a3951a8df974c82244d50
Michael Mallari <hello@michaelmallari.com>
2018-08-12 10:46:11 -0400

    Updated file structure for MVC

--- app_server/controllers/main-controller.js
+++ app_server/controllers/main-controller.js
@@ -1,7 +1,9 @@
 const home = function(req, res) {
-  res.render('default-template.ejs', { title: 'Express' });
+  res.render('default-template.ejs', {
+    title: 'Express'
+  });
 };
 
 module.exports = {
-	home
+  home
 };
